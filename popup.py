3deb166c94174f235442582c01b96deb1510ae6a
prakhar-s39 <prakharbbcc@gmail.com>
2024-09-29 19:32:48 +0530

    popup.py is working, i tried integrating it into main but not much luck, i did not commit my version of main.py yet because it might mess with other stuff, will create branch later

--- popup.py
+++ popup.py
@@ -1,12 +1,29 @@
 import flet as ft
 
-class Popup:
+class Popup(ft.Container):
     def __init__(self, title: str):
+        super().__init__()
         self.title = title
 
     def show(self, page: ft.Page):
         popup = ft.AlertDialog(
-            title=ft.Text(self.title),
+            modal=True,
+            content=ft.Container(
+                content=ft.Text(
+                    self.title,
+                    text_align=ft.TextAlign.CENTER,
+                    color="#E7F5C6",
+                    font_family="Helvetica",
+                    size=20,
+                    weight=ft.FontWeight.BOLD
+                ),
+                alignment=ft.alignment.center,
+                padding=20,  
+                width=200, 
+                height=100,  
+                border_radius=15,  
+                bgcolor="#555555"
+            ),
             on_dismiss=lambda e: page.overlay.remove(popup)
         )
         page.overlay.append(popup)
@@ -22,9 +39,10 @@ def main(page: ft.Page):
     # Button for testing
     def on_button_click(e):
         Popup("Task Created!").show(page)
-    button = ft.ElevatedButton("Create Task", on_click=on_button_click)
+
+    button = ft.IconButton(icon=ft.icons.ADD, on_click=on_button_click)
     page.add(button)
-    page.update()  
+    page.update()
 
 if __name__ == "__main__":
     ft.app(target=main)
